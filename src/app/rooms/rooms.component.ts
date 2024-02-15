fcb4ad7e5cdcc4f1453432ceaedc8504463aea70
vivekulhar <105244989+vivekulhar@users.noreply.github.com>
2024-02-15 21:38:14 +0530

    *ngFor structural directive

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Room } from './rooms';
-
+import { RoomList } from './rooms';
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
@@ -12,10 +12,38 @@ export class RoomsComponent {
   numberOfRooms = 10;
   hideRooms = false;
   rooms:Room={
-    // totalRooms:20,
-    // availableRooms:0,
-    // bookedRooms:5
+    totalRooms:20,
+    availableRooms:10,
+    bookedRooms:5
   }
+  roomList:RoomList[]=[{
+    roomNumber:1201,
+    roomType : 'Deluxe Room',
+    amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+    price:500,
+    photos:'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=600',
+    checkinTime:new Date('11-Nov-2023'),
+    checkoutTime: new Date('12-Nov-2023')
+  },
+  {
+    roomNumber:1501,
+    roomType : 'Luxury Room',
+    amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+    price:1000,
+    photos:'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=600',
+    checkinTime:new Date('11-Nov-2023'),
+    checkoutTime: new Date('12-Nov-2023')
+  },
+  {
+    roomNumber:2001,
+    roomType : 'Private Suite',
+    amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+    price:1500,
+    photos:'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=600',
+    checkinTime:new Date('11-Nov-2023'),
+    checkoutTime: new Date('12-Nov-2023')
+  } 
+]
 
   toggle(){
     this.hideRooms = !this.hideRooms;
